59a2a1189c8eb4e2f7169601b1bf843967a6afa0
konnehmamoud <konnehmamoud@gmail.com>
2025-08-06 11:51:52 -0500

    updated recipe img

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Header from "./components/Header";
+import RecipeExcerpt from "./components/RecipeExcerpt";
 import "./App.css";
 
+
 function App() {
+  const[recipes, setRecipes] = useState([])
+
+  useEffect(()=>{
+const fetchAllRecipes = async() =>{
+  try{
+   const response = await fetch('/api/recipes');
+   if(response.ok){
+    const data = await response.json()
+    setRecipes(data)
+   } else{
+    console.log("Oops-could not fetch recipe")}
+  }catch(e){
+    console.error("An error durning the request", e)
+  }
+};
+fetchAllRecipes();
+  },[]);
   return (
     <div className='recipe-app'>
       <Header />
-      <p>Your recipes here! </p>
+      {recipes.map((recipe) =>(
+        <RecipeExcerpt key={recipe.id} recipe ={recipe}/>
+      ))}
     </div>
   );
 }
